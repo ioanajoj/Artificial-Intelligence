df490a2a56bb8ac010fd0fec7637abc6965f0e35
Ioana Joj <ioanna.gheorghiu@gmail.com>
2019-03-17 18:29:52 +0200

    ceva decent

--- Cryptarithmetic/Problem.py
+++ Cryptarithmetic/Problem.py
@@ -6,68 +6,27 @@ from collections import OrderedDict
 
 class Problem:
     def __init__(self, text):
-        # first = constrained letters
-        # second = unconstrained letters
         self.__text = text
         self.__constraints = set(self.get_constraints())
-        self.__first_letters = self.get_first_letters()
-        self.__rest_letters = self.get_rest_letters()
 
         # define States
-        # self.__state_1 = State(self.__first_letters)
-        # self.__state_2 = State(self.__rest_letters)
-        self.__current_state = State(self.__first_letters, self.__rest_letters)
+        self.__config = self.get_initial_dict()
+        self.__current_state = State(self.__config)
 
         # define iterators:
-        #   iter_1 = iterator for values different from 0
-        #   iter_2 = iterator for all values
-        self.__pool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.__iter_1 = iter(itertools.permutations(self.__pool[1:], len(self.__constraints)))
-        self.__iter_2 = iter(itertools.permutations(self.__pool, self.__current_state.get_length() - len(self.__constraints)))
+        self.__pool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.__iter = iter(itertools.permutations(self.__pool, len(self.__config)))
 
     def expand(self):
         while True:
-            values_1 = next(self.__iter_1, None)
-            values_2 = next(self.__iter_2, None)
-            # print(values)
-            if values_1 and values_2 is not None:
-                self.__current_state.set_new_values(values_1, 1)
-                self.__current_state.set_new_values(values_2, 2)
-                # self.__state_1.set_new_values(values_1)
-                # self.__state_1.set_new_values(values_2)
-                print(values_1)
-                print(values_2)
+            values = next(self.__iter, None)
+            if values is not None:
+                self.__current_state.set_new_values(values)
+                print(values)
                 if self.__current_state.is_valid(self.__constraints):
                     return self.__current_state
             else:
                 break
-
-    def expand(self):
-        while True:
-            values_1 = next(self.__iter_1, None)
-            if values_1 is not None:
-                self.__current_state.set_new_values(values_1, 1)
-                values_2 = next(self.__iter_2, None)
-                if values_1 and values_2 is not None:
-                    self.__current_state.set_new_values(values_2, 2)
-                    print(values_1)
-                    print(values_2)
-                    if self.__current_state.is_valid(self.__constraints):
-                        return self.__current_state
-            else:
-                break
-
-    def expand_rest(self):
-        self.__iter_2 = iter(
-            itertools.permutations(self.__pool, self.__current_state.get_length() - len(self.__constraints)))
-        while True:
-            values_2 = next(self.__iter_2, None)
-            if values_2 is not None:
-                self.__current_state.set_new_values(values_2, 2)
-                print(values_1)
-                print(values_2)
-                if self.__current_state.is_valid(self.__constraints):
-                    return self.__current_state
 
     # solve for minus
     def check(self, current_state):
@@ -84,26 +43,29 @@ class Problem:
                 elif word == '=':
                     before_eq = False
                 elif word != '+':
-                    left_side = self.get_value(word, current_state)
+                    if operation == '+':
+                        left_side = left_side + self.get_value(word)
+                    else:
+                        left_side = left_side - self.get_value(word)
             else:
-                right_side = self.get_value(word, current_state)
-        return left_side == right_side
+                right_side = self.get_value(word)
+        if left_side == right_side:
+            return True
+        return False
 
-    @staticmethod
-    def get_value(word, current_state):
+    def get_value(self, word):
         suma = 0
         index = 0
         for letter in word[::-1]:
-            suma += current_state.get_value(letter) * 16 * index
+            suma += self.__current_state.get_value(letter) * pow(10, index)
             index += 1
         return suma
 
     def get_root(self):
         return self.__current_state
 
-    @staticmethod
-    def get_initial_dict(text):
-        new_text = ''.join(filter(str.isalpha, text))
+    def get_initial_dict(self):
+        new_text = ''.join(filter(str.isalpha, self.__text))
         return OrderedDict.fromkeys(new_text)
 
     def get_constraints(self):
@@ -114,17 +76,3 @@ class Problem:
             if len(word) > 0:
                 constraints += word[0]
         return constraints
-
-    def get_first_letters(self):
-        new_text = ''.join(filter(lambda v: self.check_filter(v), self.__text))
-        return OrderedDict.fromkeys(new_text)
-
-    def get_rest_letters(self):
-        new_text = ''.join(filter(lambda v: str.isalpha(v) and not self.check_filter(v), self.__text))
-        return OrderedDict.fromkeys(new_text)
-
-    def check_filter(self, v):
-        return str.isalpha(v) and self.__constraints.__contains__(v)
-
-    def not_check_filter(self):
-        return not self.check_filter()
--- Cryptarithmetic/UI.py
+++ Cryptarithmetic/UI.py
@@ -19,13 +19,14 @@ from Cryptarithmetic.Problem import Problem
 
 class UI:
     def __init__(self):
-        self.__problem = Problem("SEND + MORE = MONEY")
-        self.__controller = Controller(self.__problem)
+        self.__problem = None
+        self.__controller = None
 
     def printMainMenu(self):
         s = "1 - Solve SEND + MORE = MONEY.\n"
-        # s += "2 - Solve EAT + THAT = CAKE. \n"
-        # s += "3 - Solve NEVER + DRIVE = RIDE. \n"
+        s += "2 - Solve EAT + THAT = CAKE. \n"
+        s += "3 - Solve NEVER - DRIVE = RIDE. \n"
+        s += "4 - Enter custom operation. \n"
         print(s)
 
     def run(self):
@@ -37,7 +38,19 @@ class UI:
                 if command == 0:
                     run_menu = False
                 elif command == 1:
+                    self.__problem = Problem("SEND + MORE = MONEY")
+                    self.__controller = Controller(self.__problem)
                     self.solveWithDFS()
+                elif command == 2:
+                    self.__problem = Problem("EAT + THAT = CAKE")
+                    self.__controller = Controller(self.__problem)
+                    self.solveWithDFS()
+                elif command == 3:
+                    self.__problem = Problem("NEVER - DRIVE = RIDE")
+                    self.__controller = Controller(self.__problem)
+                    self.solveWithDFS()
+                elif command == 4:
+                    self.custom_operation()
             except IOError:
                 print("invalid command")
 
@@ -45,6 +58,12 @@ class UI:
         startTime = time()
         print(str(self.__controller.dfs(self.__problem.get_root())))
         print('execution time = ', time() - startTime, " seconds")
+
+    def custom_operation(self):
+        text = input("Enter your problem: ")
+        self.__problem = Problem(text)
+        self.__controller = Controller(self.__problem)
+        self.solveWithDFS()
 
 
 def main():
